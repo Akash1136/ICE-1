e098ff31a85f783fa079d1ee3a3c444e581c83bc
Akash1136 <pate3576@sheridancollege.ca>
2022-02-02 11:29:00 -0500

    Object Added

--- src/ca/sheridancollege/week2/softwarefundamentals/CardDealer.java
+++ src/ca/sheridancollege/week2/softwarefundamentals/CardDealer.java
@@ -15,7 +15,7 @@ public class CardDealer
     {
         Card nineClubs = new Card("Clbs", 9);
         nineClubs.setSuit("diamonds");
-        Card King = new Card("spades",6);
-        King.setSuit("hearts");
+        Card Queen = new Card("spades",6);
+        Queen.setSuit("hearts");
     }
 }
